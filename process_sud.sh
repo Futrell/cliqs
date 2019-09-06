f7faf7105d0415e8b7bc7dcde64713249274d117
Futrell <futrell@mit.edu>
2019-09-06 09:40:51 -0700

    fix process_sud so it chooses a corpus for every lang

--- process_sud.sh
+++ process_sud.sh
@@ -11,7 +11,7 @@ done
 
 # For each language, take the largest corpus to be its representative
 
-CHOSEN_CORPORA=$(wc -l */all.conllu | grep -v SUD | grep -v total | sed "s/\/all.conllu//g" | sed "s/_/ /g" | sort -r -k2,2 -k1,1 | awk -F" " '{if (l != $2) { print $2"_"$3; l=$2 }}' | sed '1d')
+CHOSEN_CORPORA=$(wc -l */all.conllu | grep -v SUD | grep -v total | sed "s/\/all.conllu//g" | sed "s/_/ /g" | sort -r -k2,2 -k1,1 | awk -F" " '{if (l != $2) { print $2"_"$3; l=$2 }}')
 
 for corpus in $CHOSEN_CORPORA; do
     lang=$(awk -F"_" '{print $1}' <<< $corpus)
