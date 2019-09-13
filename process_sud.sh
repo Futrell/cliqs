2f5936d98aadf609b2056b3ac7f06f4b742e2938
Futrell <richard.futrell@gmail.com>
2019-09-13 09:31:01 -0700

    bug fix on corpus selectioN

--- process_sud.sh
+++ process_sud.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Make concatenated files; make shortcodes with iso codes
 
 for lang in `ls . | grep SUD_`; do
@@ -11,7 +12,7 @@ done
 
 # For each language, take the largest corpus to be its representative
 
-CHOSEN_CORPORA=$(wc -l */all.conllu | grep -v SUD | grep -v total | sed "s/\/all.conllu//g" | sed "s/_/ /g" | sort -r -k2,2 -k1,1 | awk -F" " '{if (l != $2) { print $2"_"$3; l=$2 }}')
+CHOSEN_CORPORA=$(wc -l */all.conllu | grep -v SUD | grep -v total | sed "s/\/all.conllu//g" | sed "s/_/ /g" | sort -r -nk1,1 | sort -sk 2,2 | awk -F" " '{if (l != $2) { print $2"_"$3; l=$2 }}')
 
 for corpus in $CHOSEN_CORPORA; do
     lang=$(awk -F"_" '{print $1}' <<< $corpus)
